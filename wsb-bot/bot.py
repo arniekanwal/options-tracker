8467346ffd3d8138f56e90d1092877376003140f
arniekanwal <arnavkanwal@gmail.com>
2024-10-02 16:02:35 -0400

    init wsb/reddit scraping tool

---
+++ wsb-bot/bot.py
@@ -0,0 +1,16 @@
+# custom imports
+import sqliteConnection from data.py
+
+class RedditBot:
+    def __init__(self):
+        pass
+
+    def run(self) -> None:
+        pass
+
+    def __backfillData(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    wsb_bot = RedditBot()
+    wsb_bot.run()
---
+++ wsb-bot/data.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+class sqliteConnection:
+    def __init__(self):
+        pass
+
+    def backfillDB(self):
+        pass
+
+    def queryPopular(self):
+        pass
+
+    def insertData(self):
+        pass
+
+    def deleteOldData(self):
+        pass
+
+    def createConnection(self):
+        pass
+    
+    def stopConnection(self):
+        pass
