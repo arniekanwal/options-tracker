083918a5a7fb64522b6a04823cb72b7250947640
arniekanwal <arnavkanwal@gmail.com>
2024-10-10 22:38:02 -0400

    fix reddit client connection, add requirements.txt

--- wsb-bot/bot.py
+++ wsb-bot/bot.py
@@ -1,12 +1,13 @@
 import praw
 import os
 from dotenv import load_dotenv
-from datetime import datetime
+from datetime import datetime, timedelta
 from collections import defaultdict
+import json
 
 # nlp imports
-import spacy
-from textblob import TextBlob
+# import spacy
+# from textblob import TextBlob
 
 # custom imports
 from db_handler import Database
@@ -18,13 +19,15 @@ class RedditBot:
 
         # Initialize Reddit API client
         self.rclient = praw.Reddit(
-            client_id=os.getenv('REDDIT_ID'),
-            client_secret=os.getenv('REDDIT_SECRET'),
-            user_agent=os.getenv('APP_NAME')
+            client_id=os.getenv('REDDIT_CLIENT_ID'),
+            client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
+            user_agent=os.getenv('REDDIT_USER_AGENT'),
+            # username=os.getenv('REDDIT_USERNAME'),
+            # password=os.getenv('REDDIT_PASSWORD')
         )
 
         # Initialize NLP model
-        self.nlp = spacy.load("en_core_web_sm")
+        # self.nlp = spacy.load("en_core_web_sm")
 
         # Initialize database connection
         self.db = Database()
@@ -35,20 +38,45 @@ class RedditBot:
     def search_subreddit(self, date: datetime, subreddit: str, search_query: str):
         pass  
 
-    def get_wsb_top_posts(self, date: datetime = self._days_ago(1)):
-        '''
-        Get the most popular posts from r/wallstreetbets on a given day. 
-
-        Parameters:
-            date (datetime): Must be a past date (yesterday by default).
-
-        Returns:
-            List[praw.models.Submission]: A list of praw.models.Submission objects.
-        '''
-        # Connect to r/wallstreetbets and retrieve the top posts for the given date
+    def get_wsb_top_posts(self, date: datetime = None, limit: int = 20):
+        if date is None:
+            date = self._days_ago(1)
+        
         subreddit = self.rclient.subreddit('wallstreetbets')
-        top_posts = subreddit.top(time_filter='day', limit=None, date=date)
-        return top_posts
+        top_posts = subreddit.top(time_filter='day', limit=limit, date=date)
+        
+        posts_data = []
+        
+        for post in top_posts:
+            post_data = {
+                "post_id": post.id,
+                "title": post.title,
+                "author": str(post.author),
+                "score": post.score,
+                "url": post.url,
+                "body": post.selftext,
+                "created_utc": post.created_utc,
+                "num_comments": post.num_comments,
+                "comments": []
+            }
+            
+            post.comments.replace_more(limit=0)  # Remove MoreComments objects
+            for comment in post.comments.list():
+                comment_data = {
+                    "comment_id": comment.id,
+                    "author": str(comment.author),
+                    "score": comment.score,
+                    "body": comment.body,
+                    "created_utc": comment.created_utc
+                }
+                post_data["comments"].append(comment_data)
+            
+            posts_data.append(post_data)
+        
+        with open('top_posts.json', 'w', encoding='utf-8') as f:
+            json.dump(posts_data, f, ensure_ascii=False, indent=2)
+        
+        return posts_data
 
 
     def __backfill_database(self, days: int):
@@ -57,9 +85,10 @@ class RedditBot:
     def __stock_disambiguation(self, ticker: str) -> str:
         pass
 
-    def _days_ago(self, days: int):
+    @staticmethod
+    def _days_ago(days: int):
         '''
-        Returns the date of the day `days` ago
+        Returns the date `days` ago from today.
         '''
         if days < 0:
             raise ValueError("Days must be a non-negative integer")
@@ -67,4 +96,4 @@ class RedditBot:
 
 if __name__ == "__main__":
     wsb_bot = RedditBot()
-    wsb_bot.run()
+    wsb_bot.get_wsb_top_posts()
